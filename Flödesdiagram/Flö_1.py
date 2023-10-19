01ceefdb93b2a444feed67cbd8ea6c9168c7e5ac
Gustafpieper <Gustafpieper@gmail.com>
2023-10-19 13:40:42 +0200

    a

---
+++ Flödesdiagram/Flö_1.py
@@ -0,0 +1,7 @@
+svar=input("Skriv in ett tal: ")
+tal=int(svar)
+if tal==42:
+    print("gissade rätt")
+else:
+    print("gissade fel")
+print("klart")
---
+++ readme.md
@@ -0,0 +1,6 @@
+Loggbok
+==================
+
+19-10-23
+------------------
+Idag har jag fixat min loggbok och gjort ett prov
